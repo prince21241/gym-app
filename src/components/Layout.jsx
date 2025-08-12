b32e053ba70022f11539799b91fe95a35b9bfe9e
Prince Raval <divya.raval92@gmail.com>
2025-08-11 22:05:16 -0400

    Made  Dashboard page more visable

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,43 @@
-export default function Layout() {
-  return <></>;
+export default function Layout(props) {
+  const { children } = props;
+
+  const header = (
+    <header>
+      <h1 className="text-gradient">GYM APP</h1>
+      <p>
+        <strong>30 Simple Workout program</strong>
+      </p>
+    </header>
+  );
+
+  const footer = (
+    <footer>
+      <p>
+        Built by{" "}
+        <a
+          href="https://princeraval.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Prince Raval
+        </a>{" "}
+        using{" "}
+        <a
+          href="https://www.fantacss.smoljames.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FantaCSS
+        </a>
+      </p>
+    </footer>
+  );
+
+  return (
+    <>
+      {header}
+      {children}
+      {footer}
+    </>
+  );
 }
